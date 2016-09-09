d193627c58dac441587d66cc0c455d94d9d5dae4
brendan <brendan@debian>
2016-09-09 11:14:37 -0400

    Added analyzer file, currently empty.

--- parse.hs
+++ parse.hs
@@ -1,11 +1,15 @@
+--An LL(1) parser for arithmetic expression, the results of which will be passed to the analyzer where the
+--sign will be determined
 module Parse where
 
 data Analyzer = Oplus | Otimes
 
+-- Since parenthesization will be used to determine operation order, we need a way to distinguish
+-- a parenthesized expression so we will know what to evaluate first.
 data O_expression = O_expression Integer Analyzer O_expression | Parenthesized_o_expression
                   | Singleton Integer
 
 type Parentheszed_o_expression = O_expression
 
 parse :: [String] -> O_expression
-parse expr = undefined
+parse expr = 
