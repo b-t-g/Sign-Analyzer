9046ab9a0625844077d71d24bc92b63659172e47
Brendan Good <brendantgood@gmail.com>
2017-08-28 22:22:37 -0400

    Add option to read program from a file.

--- Main.hs
+++ Main.hs
@@ -3,4 +3,12 @@ import System.Environment
 import Parse
 
 main :: IO ()
-main = getArgs >>= \args -> putStrLn (args !! 0) >> putStrLn (Parse.readExpr (args !! 0) )
+main = getArgs >>= \args -> if length args > 1 && (args !! 0) == "-f" then
+                              readFile(args !! 1) >>= \file -> putStrLn(Parse.readExpr file)
+                            else
+                              if length args == 1 then
+                                putStrLn (Parse.readExpr (args !! 0) )
+                              else
+                                mapM_ putStr ["Input does not match expected format.\n",
+                                              "Expecting either -f followed by a file ",
+                                              "or a single argument consisting of a program.\n"]
