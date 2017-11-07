05f39b536b7f807ea28eaff4d12a7413775e4857
Brendan Good <brendantgood@gmail.com>
2017-11-06 22:42:18 -0500

    Fix hlint errors in ArithmeticExpression.hs and Main.hs

--- ArithmeticExpression.hs
+++ ArithmeticExpression.hs
@@ -11,14 +11,14 @@ data Operator = Oplus | Otimes deriving Eq
 data Sign = Plus | Minus | Zero deriving (Eq, Ord)
 
 instance Show ArithmeticExpression where
-  show (ArithmeticExpression a1 o a2) = show a1 ++ " " ++ (show o) ++ " " ++ show a2
+  show (ArithmeticExpression a1 o a2) = show a1 ++ " " ++ show o ++ " " ++ show a2
   show (Singleton i) = show i
   show (Var s)       = s
 
 
 instance Show Operator where
-  show (Oplus) = "\8853"
-  show (Otimes) = "\8855"
+  show Oplus = "\8853"
+  show Otimes = "\8855"
 
 instance Show Sign where
   show Plus = "{+}"
--- Main.hs
+++ Main.hs
@@ -3,11 +3,11 @@ import System.Environment
 import Parse
 
 main :: IO ()
-main = getArgs >>= \args -> if length args > 1 && (args !! 0) == "-f" then
+main = getArgs >>= \args -> if length args > 1 && head args == "-f" then
                               readFile(args !! 1) >>= \file -> putStrLn(Parse.readExpr file)
                             else
                               if length args == 1 then
-                                putStrLn (Parse.readExpr (args !! 0) )
+                                putStrLn (Parse.readExpr (head args))
                               else
                                 mapM_ putStr ["Input does not match expected format.\n",
                                               "Expecting either -f followed by a file ",
