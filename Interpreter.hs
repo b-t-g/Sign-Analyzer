fb1f5e14900f32bd7b1abddfd7fba34cec1786d6
Brendan Good <brendantgood@gmail.com>
2017-12-21 10:26:05 -0500

    Remove Interpreter.hs (empty file)

--- Interpreter.hs
+++
@@ -1,8 +0,0 @@
-module Interpreter where
-import Expressions
-
-
-{- Do I need to make a new parser for these expressions?
-   Yeah, it appears to be that way.
-   Oh, but that's not too bad, it appears everything else is
-   hella LL(1). -}
